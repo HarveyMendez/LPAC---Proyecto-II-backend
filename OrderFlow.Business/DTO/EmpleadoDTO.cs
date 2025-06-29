10a48b76f751c00dec96800a31778c1a093bcc28
HarveyMendez <harvey.mendez@ucr.ac.cr>
2025-06-29 15:50:11 -0600

    se movieron los DTOs de lugar

---
+++ OrderFlow.Business/DTO/EmpleadoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderFlow.API.DTO
+{
+    public class EmpleadoDTO
+    {
+        public int idEmpleado { get; set; }
+        public string nombreEmpleado { get; set; } = string.Empty;
+        public string apellidosEmpleado { get; set; } = string.Empty;
+        public string puesto { get; set; } = string.Empty;
+        public string extension { get; set; } = string.Empty;
+        public string telefonoTrabajo { get; set; } = string.Empty;
+        public DepartamentoDTO departamento { get; set; } = new DepartamentoDTO();
+        public EmpleadoDTO empleado { get; set; } = new EmpleadoDTO();
+    }
+}
